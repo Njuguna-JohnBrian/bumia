d73631eaa48a5ebff6ba696ed4af68b899f3becd
John Brian <njugunajb96@gmail.com>
2023-02-03 12:40:26 +0300

    setting up tools

---
+++ backend/config/database.ts
@@ -0,0 +1,13 @@
+import * as mongoose from "mongoose";
+
+const connectDatabase = async () => {
+  mongoose.set("strictQuery", false);
+
+  await mongoose.connect(process.env.DB_LOCAL_URI).then((connection) => {
+    console.log(
+      `MongoDB Database Connected to Host: ${connection.connection.host}`
+    );
+  });
+};
+
+export { connectDatabase };
