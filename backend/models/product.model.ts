1ffd243fb1764f920bb7a768e8d151bbae4b7917
John Brian <njugunajb96@gmail.com>
2023-02-03 20:55:25 +0300

    update product

--- backend/models/product.model.ts
+++ backend/models/product.model.ts
@@ -38,7 +38,44 @@ const productSchema = new Schema<IProduct>({
   category: {
     type: String,
     required: [true, "Please select the category for this product"],
-    enum: Object.values(EProducts),
+    enum: {
+      values: [Object.values(EProducts)],
+      message: "Please select the category for this product",
+    },
+  },
+  seller: {
+    type: String,
+    required: [true, "Please enter product seller"],
+  },
+  stock: {
+    type: Number,
+    required: [true, "Please enter product stock"],
+    maxlength: [5, "Product stock cannot exceed 5 characters"],
+    default: 0.0,
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: String,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
 });
 
