2743ff08fd3dca6262b7d6a138fd65b09f6156ce
John Brian <njugunajb96@gmail.com>
2023-02-03 12:40:26 +0300

    setting up tools

---
+++ backend/server.ts
@@ -0,0 +1,15 @@
+import * as dotenv from "dotenv";
+import * as path from "path";
+dotenv.config({
+  path: path.join(__dirname, "../../backend/config/config.env"),
+});
+import { app } from "./app";
+import { connectDatabase } from "./config/database";
+
+connectDatabase();
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server running on PORT: ${process.env.PORT} in ${process.env.NODE_ENV}`
+  );
+});
