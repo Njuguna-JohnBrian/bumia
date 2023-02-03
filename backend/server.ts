dc9a2fc29b4793ac527d5023c1c2766c310a52c9
John Brian <njugunajb96@gmail.com>
2023-02-03 12:44:02 +0300

    indent code

--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,7 @@ import * as path from "path";
 dotenv.config({
   path: path.join(__dirname, "../../backend/config/config.env"),
 });
+
 import { app } from "./app";
 import { connectDatabase } from "./config/database";
 
