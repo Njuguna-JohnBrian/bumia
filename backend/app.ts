73c3da26525db73fb78dc010b800f9f2d21ad559
John Brian <njugunajb96@gmail.com>
2023-02-06 17:08:47 +0300

    mount products

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 
 import { health } from "./routes/health.router";
+import { product } from "./routes/product.router";
 
 const app: Express = express();
 
@@ -9,5 +10,7 @@ app.use(express.json());
 const baseUrl = "/bumia/v1";
 
 app.use(baseUrl, health);
+
+app.use(baseUrl, product);
 
 export { app };
