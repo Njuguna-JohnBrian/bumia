c39aa7dc804934cba511696ee7f43b01c3db9a7e
John Brian <njugunajb96@gmail.com>
2023-02-06 15:50:41 +0300

    alter health router

--- backend/routes/health.ts
+++ backend/routes/health.ts
@@ -1,10 +1,10 @@
 import * as express from "express";
-import { ProductController } from "../controllers/health";
+import { HealthController } from "../controllers/health";
 
 const health: express.Router = express.Router();
 
 health.get("/health", (req, res) => {
-  ProductController.checkHealth(req, res);
+  HealthController.checkHealth(req, res);
 });
 
 export { health };
