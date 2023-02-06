7d0b25ee62cfaa6c686a75e8ad2b971645db72dd
John Brian <njugunajb96@gmail.com>
2023-02-06 15:50:32 +0300

    alter health controller

--- backend/controllers/health.ts
+++ backend/controllers/health.ts
@@ -1,9 +1,10 @@
 import { Response, Request } from "express";
 
-export class ProductController {
+export class HealthController {
   static async checkHealth(req: Request, res: Response) {
     return res.status(200).json({
       success: true,
+      message: "System is operational and healthy",
     });
   }
 }
