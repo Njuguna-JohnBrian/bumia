f02e57214d83bd56833876ab319638af55a176ce
John Brian <njugunajb96@gmail.com>
2023-02-06 14:54:08 +0300

    add seed data

---
+++ backend/data/products.data.ts
@@ -0,0 +1,94 @@
+const data = [
+  {
+    name: "ScanDisk Ultra",
+    price: "45.08",
+    description:
+      "The SanDisk Ultra® microSD™ UHS-I card gives you the freedom to shoot, save and share more than ever before. With capacities up to 512GB1, our SanDisk Ultra microSD™ card has room for even more hours of Full HD video3 and delivers transfer speeds of up to 100MB/s2 to help you move that content fast.",
+    ratings: 4.5,
+    images: [
+      {
+        public_id: "1",
+        url: "https://images.unsplash.com/photo-1611153730462-e84a16b8c6e1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzUwNjB8MHwxfHNlYXJjaHw5fHxzY2FkaXNrJTIwbWVtb3J5JTIwY2FyZHxlbnwwfHx8fDE2MzkwMTYwNDU&ixlib=rb-1.2.1&q=80&w=1080",
+      },
+    ],
+    category: "Electronics",
+    seller: "Ebay",
+    stock: 50,
+    numOfReviews: 32,
+    reviews: [],
+  },
+  {
+    name: "Hp Laptop",
+    price: "96.08",
+    description:
+      "HP NoteBook is a Windows 10 laptop with a 15.60-inch display that has a resolution of 1366x768 pixels. It is powered by a Core i5 processor and it comes with 8GB of RAM. The HP NoteBook packs 256GB of SSD storage. Graphics are powered by Intel HD Graphics 620.",
+    ratings: 4.5,
+    images: [
+      {
+        public_id: "2",
+        url: "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzUwNjB8MHwxfHNlYXJjaHwzfHxsYXB0b3B8ZW58MHx8fHwxNjM5MDE1OTM0&ixlib=rb-1.2.1&q=80&w=1080",
+      },
+    ],
+    category: "Laptop",
+    seller: "Amazon",
+    stock: 50,
+    numOfReviews: 32,
+    reviews: [],
+  },
+  {
+    name: "The 5AM Club",
+    price: "45.08",
+    description:
+      " The 5 AM Club helps you get up at 5 AM every morning, build a morning routine, and make time for the self-improvement you need to find success. You wake up to your alarm, press snooze, and roll back over for an extra few minutes of sleep",
+    ratings: 4.5,
+    images: [
+      {
+        public_id: "3",
+        url: "https://images.unsplash.com/photo-1434652917459-36f6692da944?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNzUwNjB8MHwxfHNlYXJjaHwxMHx8NWFtJTIwY2x1YiUyMGJvb2t8ZW58MHx8fHwxNjM5MDE1NzE2&ixlib=rb-1.2.1&q=80&w=1080",
+      },
+    ],
+    category: "Books",
+    seller: "Amazon",
+    stock: 50,
+    numOfReviews: 30,
+    reviews: [],
+  },
+  {
+    name: "Basketball Ball",
+    price: "45.08",
+    description:
+      "A basketball is a spherical ball used in basketball games. Basketballs usually range in size from very small promotional items that are only a few inches (some centimeters) in diameter to extra large balls nearly 2 feet (60 cm) in diameter used in training exercises.",
+    ratings: 4.5,
+    images: [
+      {
+        public_id: "4",
+        url: "https://tinyurl.com/2p8d55es",
+      },
+    ],
+    category: "Sports",
+    seller: "Ebay",
+    stock: 50,
+    numOfReviews: 32,
+    reviews: [],
+  },
+  {
+    name: "Basketball",
+    price: "45.08",
+    description:
+      "A basketball is a spherical ball used in basketball games. Basketballs usually range in size from very small promotional items that are only a few inches (some centimeters) in diameter to extra large balls nearly 2 feet (60 cm) in diameter used in training exercises.",
+    ratings: 4.5,
+    images: [
+      {
+        public_id: "4",
+        url: "https://tinyurl.com/2p8d55es",
+      },
+    ],
+    category: "Sports",
+    seller: "Ebay",
+    stock: 50,
+    numOfReviews: 32,
+    reviews: [],
+  },
+];
+
+export { data };
