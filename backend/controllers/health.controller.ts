f3c7cbe5507631f6868962762611198e641e1d63
John Brian <njugunajb96@gmail.com>
2023-02-06 17:08:54 +0300

    add health

--- backend/controllers/health.controller.ts
+++ backend/controllers/health.controller.ts
@@ -1,6 +1,14 @@
 import { Response, Request } from "express";
 
 export class HealthController {
+  /**Check Bumia Health
+   *
+   * /health
+   *
+   * @param {*} req request body with `id` as parameter
+   * @param {*} res response body with the found product
+   * @param {*} next next controller to take over execution
+   */
   static async checkHealth(req: Request, res: Response) {
     return res.status(200).json({
       success: true,
