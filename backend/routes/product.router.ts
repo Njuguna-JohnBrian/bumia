612b64c6d4f96aa0c6038b323b99f389c70592d6
John Brian <njugunajb96@gmail.com>
2023-02-07 10:03:30 +0300

    get single product

--- backend/routes/product.router.ts
+++ backend/routes/product.router.ts
@@ -5,8 +5,20 @@ const product: express.Router = express.Router();
 
 const productBaseUrl = "/product";
 
+const productsBaseUrl = "/products";
+
+/**
+ *Get single product
+ */
 product.get(`${productBaseUrl}/:id`, (req, res) => {
   ProductController.getSingleProduct(req, res);
+});
+
+/**
+ * Get all products
+ */
+product.get(productsBaseUrl, (req, res) => {
+  ProductController.getAllProducts(req, res);
 });
 
 export { product };
