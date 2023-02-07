609df40c6e3b3c0229bf94242cc2b6bee20f0f54
John Brian <njugunajb96@gmail.com>
2023-02-07 10:54:51 +0300

    update product

--- backend/controllers/product.controller.ts
+++ backend/controllers/product.controller.ts
@@ -68,4 +68,37 @@ export class ProductController {
       product,
     });
   }
+
+  /**Update Product
+   *
+   * /product/:id
+   *
+   * @param {*} req request body with new product details
+   * @param {*} res response body with the new product
+   *
+   */
+  static async updateProduct(
+    req: Request,
+    res: Response
+  ): Promise<Response<object, Record<string, object>>> {
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: `Product by id: ${req.params.id} not found`,
+      });
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+
+    return res.status(200).json({
+      success: true,
+      product,
+    });
+  }
 }
