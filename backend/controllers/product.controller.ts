afaa9de0320ac42f9a81a8cb553beed656532f55
John Brian <njugunajb96@gmail.com>
2023-02-10 11:44:14 +0300

    remove product class

--- backend/controllers/product.controller.ts
+++ backend/controllers/product.controller.ts
@@ -1,165 +1,159 @@
 import { Response, Request } from "express";
 import { Product } from "../models/product.model";
 
-export class ProductController {
-  /**Get Single Product
-   *
-   * /product/:`id`
-   *
-   * @param {*} req request body with `id` as parameter
-   * @param {*} res response body with the found product
-   *
-   */
-  static async getSingleProduct(
-    req: Request,
-    res: Response
-  ): Promise<Response<object, Record<string, object>>> {
-    try {
-      const product = await Product.findById(req.params.id);
+/**Get Single Product
+ *
+ * /product/:`id`
+ *
+ * @param {*} req request body with `id` as parameter
+ * @param {*} res response body with the found product
+ *
+ */
+const getSingleProduct = async (req: Request, res: Response) => {
+  try {
+    const product = await Product.findById(req.params.id);
 
-      if (!product) {
-        return res.status(404).json({
-          success: false,
-          message: `Product by id: ${req.params.id} not found`,
-        });
-      }
-
-      return res.status(200).json({
-        success: true,
-        product,
-      });
-    } catch (error) {
-      return res.status(500).json({
+    if (!product) {
+      return res.status(404).json({
         success: false,
-        message: "Internal server error",
+        message: `Product by id: ${req.params.id} not found`,
       });
     }
+
+    return res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
   }
+};
 
-  /**Get All Products
-   *
-   * /products
-   *
-   * @param {*} req request body
-   * @param {*} res response body with the found products
-   *
-   */
-  static async getAllProducts(
-    req: Request,
-    res: Response
-  ): Promise<Response<object, Record<string, object>>> {
-    try {
-      const products = await Product.find();
+/**Get All Products
+ *
+ * /products
+ *
+ * @param {*} req request body
+ * @param {*} res response body with the found products
+ *
+ */
+const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await Product.find();
 
-      return res.status(200).json({
-        success: true,
-        products,
-      });
-    } catch (error) {
-      return res.status(500).json({
+    return res.status(200).json({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+/**Create Product
+ *
+ * /product/new
+ *
+ * @param {*} req request body with product details
+ * @param {*} res response body with the new product
+ *
+ */
+const createProduct = async (req: Request, res: Response) => {
+  try {
+    const product = await Product.create(req.body);
+
+    return res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+/**Update Product
+ *
+ * /product/:id
+ *
+ * @param {*} req request body with new product details
+ * @param {*} res response body with the new product
+ *
+ */
+const updateProduct = async (req: Request, res: Response) => {
+  try {
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
         success: false,
-        message: "Internal server error",
+        message: `Product by id: ${req.params.id} not found`,
       });
     }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+
+    return res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
   }
+};
 
-  /**Create Product
-   *
-   * /product/new
-   *
-   * @param {*} req request body with product details
-   * @param {*} res response body with the new product
-   *
-   */
-  static async createProduct(
-    req: Request,
-    res: Response
-  ): Promise<Response<object, Record<string, object>>> {
-    try {
-      const product = await Product.create(req.body);
+/**Delete Product
+ *
+ * /product/:id
+ *
+ * @param {*} req request body with product to delete
+ * @param {*} res response body with delete message
+ *
+ */
+const deleteProduct = async (req: Request, res: Response) => {
+  try {
+    const product = await Product.findById(req.params.id);
 
-      return res.status(200).json({
-        success: true,
-        product,
-      });
-    } catch (error) {
-      return res.status(500).json({
+    if (!product) {
+      return res.status(404).json({
         success: false,
-        message: "Internal server error",
+        message: `Product by id: ${req.params.id} not found`,
       });
     }
+
+    await product.remove();
+
+    return res.status(200).json({
+      success: true,
+      message: `Product by id: ${req.params.id} deleted`,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
   }
+};
 
-  /**Update Product
-   *
-   * /product/:id
-   *
-   * @param {*} req request body with new product details
-   * @param {*} res response body with the new product
-   *
-   */
-  static async updateProduct(
-    req: Request,
-    res: Response
-  ): Promise<Response<object, Record<string, object>>> {
-    try {
-      let product = await Product.findById(req.params.id);
-
-      if (!product) {
-        return res.status(404).json({
-          success: false,
-          message: `Product by id: ${req.params.id} not found`,
-        });
-      }
-
-      product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
-        useFindAndModify: false,
-      });
-
-      return res.status(200).json({
-        success: true,
-        product,
-      });
-    } catch (error) {
-      return res.status(500).json({
-        success: false,
-        message: "Internal server error",
-      });
-    }
-  }
-
-  /**Delete Product
-   *
-   * /product/:id
-   *
-   * @param {*} req request body with product to delete
-   * @param {*} res response body with delete message
-   *
-   */
-  static async deleteProduct(req: Request, res: Response) {
-    try {
-      const product = await Product.findById(req.params.id);
-
-      if (!product) {
-        return res.status(404).json({
-          success: false,
-          message: `Product by id: ${req.params.id} not found`,
-        });
-      }
-
-      await product.remove();
-
-      return res.status(200).json({
-        success: true,
-        message: `Product by id: ${req.params.id} deleted`,
-      });
-    } catch (error) {
-      return res.status(500).json({
-        success: false,
-        message: "Internal server error",
-      });
-    }
-  }
-}
+export {
+  getSingleProduct,
+  getAllProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
