cd4fd61dd5a06e8a489c80012c9d0ca47b833bcd
John Brian <njugunajb96@gmail.com>
2023-02-10 15:43:42 +0300

    switch to enum values

--- backend/controllers/health.controller.ts
+++ backend/controllers/health.controller.ts
@@ -2,6 +2,10 @@ import { Response, Request, NextFunction } from "express";
 
 import { CatchAsyncErrors } from "../middlewares/catchAsyncErrors.middleware";
 import { ErrorHandler } from "../utils/errorHandler.utils.";
+import {
+  BumiaDefinitions,
+  ControllerMessages,
+} from "../interfaces/error.interface";
 
 /**Check Bumia Health
  *
@@ -14,11 +18,11 @@ import { ErrorHandler } from "../utils/errorHandler.utils.";
 const checkHealth = CatchAsyncErrors(
   async (req: Request, res: Response, next: NextFunction) => {
     if (res.errored) {
-      return next(new ErrorHandler("Internal server error", 500));
+      return next(new ErrorHandler(BumiaDefinitions.INTERNAL_ERROR, 500));
     }
     return res.status(200).json({
       success: true,
-      message: "System is operational and healthy",
+      message: ControllerMessages.HEALTH_MESSAGE,
     });
   }
 );
