7a791984a59514ed2111ebd2b0a6f22967fd79c6
John Brian <njugunajb96@gmail.com>
2023-02-10 11:31:36 +0300

    add return statement

--- backend/routes/product.router.ts
+++ backend/routes/product.router.ts
@@ -11,35 +11,35 @@ const productsBaseUrl = "/products";
  *Get single product
  */
 product.get(`${productBaseUrl}/:id`, (req, res) => {
-  ProductController.getSingleProduct(req, res);
+  return ProductController.getSingleProduct(req, res);
 });
 
 /**
  * Get all products
  */
 product.get(productsBaseUrl, (req, res) => {
-  ProductController.getAllProducts(req, res);
+  return ProductController.getAllProducts(req, res);
 });
 
 /**
  * Create new product
  */
 product.post(`${productBaseUrl}/new`, (req, res) => {
-  ProductController.createProduct(req, res);
+  return ProductController.createProduct(req, res);
 });
 
 /**
  * Update product
  */
 product.put(`${productBaseUrl}/:id`, (req, res) => {
-  ProductController.updateProduct(req, res);
+  return ProductController.updateProduct(req, res);
 });
 
 /**
  * Delete product
  */
 product.delete(`${productBaseUrl}/:id`, (req, res) => {
-  ProductController.deleteProduct(req, res);
+  return ProductController.deleteProduct(req, res);
 });
 
 export { product };
