95d63cce3661edafa41ef9a52883d6d017c40260
John Brian <njugunajb96@gmail.com>
2023-02-10 11:44:26 +0300

    remove class ref

--- backend/routes/health.router.ts
+++ backend/routes/health.router.ts
@@ -1,12 +1,12 @@
 import * as express from "express";
-import { HealthController } from "../controllers/health.controller";
+import { checkHealth } from "../controllers/health.controller";
 
 const health: express.Router = express.Router();
 
 const healthBaseUrl = "/health";
 
 health.get(`${healthBaseUrl}`, (req, res) => {
-  return HealthController.checkHealth(req, res);
+  return checkHealth(req, res);
 });
 
 export { health };
