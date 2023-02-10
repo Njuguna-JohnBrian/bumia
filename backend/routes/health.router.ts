1b3c72cf373f0f5c4be3090d230674b2e8147cd0
John Brian <njugunajb96@gmail.com>
2023-02-10 13:08:31 +0300

    indent

--- backend/routes/health.router.ts
+++ backend/routes/health.router.ts
@@ -1,12 +1,15 @@
-import * as express from "express";
+import express, { Response, Request, NextFunction } from "express";
 import { checkHealth } from "../controllers/health.controller";
 
 const health: express.Router = express.Router();
 
 const healthBaseUrl = "/health";
 
-health.get(`${healthBaseUrl}`, (req, res) => {
-  return checkHealth(req, res);
-});
+health.get(
+  `${healthBaseUrl}`,
+  (req: Request, res: Response, next: NextFunction) => {
+    return checkHealth(req, res, next);
+  }
+);
 
 export { health };
