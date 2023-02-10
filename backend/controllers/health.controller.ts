ebda5b8456554798f44d9f57b794ce8f14871e6c
John Brian <njugunajb96@gmail.com>
2023-02-10 11:44:03 +0300

    remove class definition

--- backend/controllers/health.controller.ts
+++ backend/controllers/health.controller.ts
@@ -1,25 +1,25 @@
 import { Response, Request } from "express";
 
-export class HealthController {
-  /**Check Bumia Health
-   *
-   * /health
-   *
-   * @param {*} req request body with `id` as parameter
-   * @param {*} res response body with the found product
-   * @param {*} next next controller to take over execution
-   */
-  static async checkHealth(req: Request, res: Response) {
-    try {
-      return res.status(200).json({
-        success: true,
-        message: "System is operational and healthy",
-      });
-    } catch (error) {
-      return res.status(500).json({
-        success: false,
-        message: "Internal server error",
-      });
-    }
+/**Check Bumia Health
+ *
+ * /health
+ *
+ * @param {*} req request body with `id` as parameter
+ * @param {*} res response body with the found product
+ * @param {*} next next controller to take over execution
+ */
+const checkHealth = (req: Request, res: Response) => {
+  try {
+    return res.status(200).json({
+      success: true,
+      message: "System is operational and healthy",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
   }
-}
+};
+
+export { checkHealth };
