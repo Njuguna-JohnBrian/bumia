92948bee665a553814d96a54741c1cbfbcaf436b
John Brian <njugunajb96@gmail.com>
2023-02-10 12:09:19 +0300

    wrap with async handler

--- backend/controllers/health.controller.ts
+++ backend/controllers/health.controller.ts
@@ -1,4 +1,6 @@
-import { Response, Request } from "express";
+import { Response, Request, NextFunction } from "express";
+
+import { CatchAsyncErrors } from "../middlewares/catchAsyncErrors.middlewares";
 
 /**Check Bumia Health
  *
@@ -8,18 +10,13 @@ import { Response, Request } from "express";
  * @param {*} res response body with the found product
  * @param {*} next next controller to take over execution
  */
-const checkHealth = (req: Request, res: Response) => {
-  try {
+const checkHealth = CatchAsyncErrors(
+  async (req: Request, res: Response, next: NextFunction) => {
     return res.status(200).json({
       success: true,
       message: "System is operational and healthy",
     });
-  } catch (error) {
-    return res.status(500).json({
-      success: false,
-      message: "Internal server error",
-    });
   }
-};
+);
 
 export { checkHealth };
