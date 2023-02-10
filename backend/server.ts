0a7267c054a69df8053ed1096d1152ae0ea090f6
John Brian <njugunajb96@gmail.com>
2023-02-10 14:20:57 +0300

    catch promise rejections

--- backend/server.ts
+++ backend/server.ts
@@ -7,10 +7,22 @@ dotenv.config({
 import { app } from "./app";
 import { connectDatabase } from "./config/database";
 
+process.on("uncaughtException", (err: Error) => {
+  console.log(`Error: ${err.message}`);
+  console.log("Shutting down due to Uncaught Exception");
+  process.exit(1);
+});
+
 connectDatabase();
 
 app.listen(process.env.PORT, () => {
   console.log(
     `Server running on PORT: ${process.env.PORT} in ${process.env.NODE_ENV}`
   );
+});
+
+process.on("unhandledRejection", (err: Error) => {
+  console.log(`Error: ${err.message}`);
+  console.log("Shutting down due to Unhandled Promise Rejections");
+  process.exit(1);
 });
