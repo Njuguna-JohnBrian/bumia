f266037d9097e15ec706f51233f53af289af0174
John Brian <njugunajb96@gmail.com>
2023-02-10 11:44:36 +0300

    remove product class ref

--- backend/routes/product.router.ts
+++ backend/routes/product.router.ts
@@ -1,5 +1,11 @@
 import * as express from "express";
-import { ProductController } from "../controllers/product.controller";
+import {
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+} from "../controllers/product.controller";
 
 const product: express.Router = express.Router();
 
@@ -11,35 +17,35 @@ const productsBaseUrl = "/products";
  *Get single product
  */
 product.get(`${productBaseUrl}/:id`, (req, res) => {
-  return ProductController.getSingleProduct(req, res);
+  return getSingleProduct(req, res);
 });
 
 /**
  * Get all products
  */
 product.get(productsBaseUrl, (req, res) => {
-  return ProductController.getAllProducts(req, res);
+  return getAllProducts(req, res);
 });
 
 /**
  * Create new product
  */
 product.post(`${productBaseUrl}/new`, (req, res) => {
-  return ProductController.createProduct(req, res);
+  return createProduct(req, res);
 });
 
 /**
  * Update product
  */
 product.put(`${productBaseUrl}/:id`, (req, res) => {
-  return ProductController.updateProduct(req, res);
+  return updateProduct(req, res);
 });
 
 /**
  * Delete product
  */
 product.delete(`${productBaseUrl}/:id`, (req, res) => {
-  return ProductController.deleteProduct(req, res);
+  return deleteProduct(req, res);
 });
 
 export { product };
