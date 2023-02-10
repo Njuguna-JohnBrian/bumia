0a00c1632a6b89727436ff234d3f86f6997edd0c
John Brian <njugunajb96@gmail.com>
2023-02-10 14:46:51 +0300

    fix

--- backend/middlewares/errors.middleware.ts
+++ backend/middlewares/errors.middleware.ts
@@ -1,14 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Response } from "express";
 import { ErrorHandler } from "../utils/errorHandler.utils.";
+import { IError } from "../interfaces/error.interface";
 
 const errorMiddleware = (
-  err: any,
+  err: IError,
   req: unknown,
   res: Response,
   next: NextFunction
 ) => {
-  err.statusCode = err.stausCode || 500;
+  err.statusCode = err.statusCode || 500;
 
   if (process.env.NODE_ENV === "DEVELOPMENT") {
     return res.status(err.statusCode).json({
@@ -31,9 +32,7 @@ const errorMiddleware = (
     }
 
     if (err.name === "ValidationError") {
-      const message = Object.values(err.errors).map(
-        (value: any) => value.message
-      );
+      const message = Object.values(err.errors).map((value) => value.message);
 
       error = new ErrorHandler(String(message), 400);
     }
