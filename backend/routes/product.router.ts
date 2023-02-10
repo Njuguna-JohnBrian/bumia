c6efed7879aeacc50bf764b758e614e9ce84b5c3
John Brian <njugunajb96@gmail.com>
2023-02-10 13:08:46 +0300

    add next function

--- backend/routes/product.router.ts
+++ backend/routes/product.router.ts
@@ -1,4 +1,4 @@
-import * as express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import {
   createProduct,
   deleteProduct,
@@ -16,36 +16,51 @@ const productsBaseUrl = "/products";
 /**
  *Get single product
  */
-product.get(`${productBaseUrl}/:id`, (req, res) => {
-  return getSingleProduct(req, res);
-});
+product.get(
+  `${productBaseUrl}/:id`,
+  (req: Request, res: Response, next: NextFunction) => {
+    return getSingleProduct(req, res, next);
+  }
+);
 
 /**
  * Get all products
  */
-product.get(productsBaseUrl, (req, res) => {
-  return getAllProducts(req, res);
-});
+product.get(
+  productsBaseUrl,
+  (req: Request, res: Response, next: NextFunction) => {
+    return getAllProducts(req, res, next);
+  }
+);
 
 /**
  * Create new product
  */
-product.post(`${productBaseUrl}/new`, (req, res) => {
-  return createProduct(req, res);
-});
+product.post(
+  `${productBaseUrl}/new`,
+  (req: Request, res: Response, next: NextFunction) => {
+    return createProduct(req, res, next);
+  }
+);
 
 /**
  * Update product
  */
-product.put(`${productBaseUrl}/:id`, (req, res) => {
-  return updateProduct(req, res);
-});
+product.put(
+  `${productBaseUrl}/:id`,
+  (req: Request, res: Response, next: NextFunction) => {
+    return updateProduct(req, res, next);
+  }
+);
 
 /**
  * Delete product
  */
-product.delete(`${productBaseUrl}/:id`, (req, res) => {
-  return deleteProduct(req, res);
-});
+product.delete(
+  `${productBaseUrl}/:id`,
+  (req: Request, res: Response, next: NextFunction) => {
+    return deleteProduct(req, res, next);
+  }
+);
 
 export { product };
