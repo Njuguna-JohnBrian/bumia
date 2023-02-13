d1b4a19938d73a5cd643962bf861fb2568deda76
John Brian <njugunajb96@gmail.com>
2023-02-13 15:30:02 +0300

    add pagination functionality

--- backend/controllers/product.controller.ts
+++ backend/controllers/product.controller.ts
@@ -42,7 +42,9 @@ const getSingleProduct = CatchAsyncErrors(
  *
  */
 const getAllProducts = CatchAsyncErrors(async (req: Request, res: Response) => {
-  const apiFeatures = new ApiFeatures(Product.find(), req.query).search();
+  const apiFeatures = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .pagination(4);
 
   const products = await apiFeatures.query;
 
