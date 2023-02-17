2280a64e5320bffd0e14d991022e041ccffa86e1
John Brian <njugunajb96@gmail.com>
2023-02-17 09:36:39 +0300

    mount auth routes

--- backend/app.ts
+++ backend/app.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 
 import { health } from "./routes/health.router";
 import { product } from "./routes/product.router";
+import { auth } from "./routes/auth.router";
 import errorMiddleware from "./middlewares/errors.middleware";
 
 const app: Express = express();
@@ -13,6 +14,8 @@ const baseUrl = "/bumia/v1";
 app.use(baseUrl, health);
 
 app.use(baseUrl, product);
+
+app.use(baseUrl, auth);
 
 app.use(errorMiddleware);
 
