50da678d44a5439e565c2094b3a94bda304d70c1
John Brian <njugunajb96@gmail.com>
2023-02-20 16:52:47 +0300

    send email util function

---
+++ backend/utils/sendEmail.utils.ts
@@ -0,0 +1,29 @@
+import * as nodemailer from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
+
+interface IEmailOptions {
+  email: string;
+  subject: string;
+  message: string;
+}
+
+const sendEmail = async (options: IEmailOptions) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.PORT),
+    auth: {
+      user: process.env.SMTP_EMAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  } as SMTPTransport.Options);
+  const message = {
+    from: `${process.env.SMTP_FROM_NAME} <${process.env.SMTP_FROM_EMAIL}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  return await transporter.sendMail(message);
+};
+
+export { sendEmail };
