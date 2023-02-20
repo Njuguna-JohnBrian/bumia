9db8e9d451ee9aaab9481b6ed4984bfac27f46b5
John Brian <njugunajb96@gmail.com>
2023-02-20 11:32:56 +0300

    add presave methods

--- backend/models/users.model.ts
+++ backend/models/users.model.ts
@@ -1,5 +1,9 @@
 import * as mongoose from "mongoose";
 import * as validator from "validator";
+import * as bcryptjs from "bcryptjs";
+import * as jwt from "jsonwebtoken";
+import * as crypto from "crypto";
+
 import { IUser } from "../interfaces/user.interface";
 
 const userSchema = new mongoose.Schema<IUser>(
@@ -14,6 +18,12 @@ const userSchema = new mongoose.Schema<IUser>(
       required: [true, "Please enter your email"],
       unique: true,
       validate: [validator.default.isEmail, "Please provide a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+      minlength: [6, "Your password must be longer than 6 characters"],
+      select: false,
     },
     avatar: {
       public_id: {
@@ -38,7 +48,57 @@ const userSchema = new mongoose.Schema<IUser>(
   }
 );
 
-const User = mongoose.model("User", userSchema);
+/**
+ * Encrypt user password before saving
+ */
+
+userSchema.pre("save", async function (next) {
+  const SALT_WORK_FACTOR = 10;
+
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcryptjs.genSalt(SALT_WORK_FACTOR);
+
+  const hash = await bcryptjs.hash(this.password, salt);
+
+  this.password = hash;
+
+  next();
+});
+
+/**
+ * Compare user password with db password
+ */
+userSchema.methods.comparePassword = async function (enteredPassword: string) {
+  return await bcryptjs.compare(enteredPassword, this.password);
+};
+
+/**
+ * Return JWT
+ */
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this.id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_TIME,
+  });
+};
+
+/**
+ * Generate password reset token
+ */
+
+userSchema.methods.getResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.resetPasswordExpire = Date.now() + 30 * 60 * 100;
+
+  return resetToken;
+};
+const User = mongoose.model<IUser>("User", userSchema);
 
 User.createIndexes();
 
