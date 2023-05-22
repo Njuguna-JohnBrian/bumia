4489c0df8e378adf641d634aa8a39c067a1cf70c
John Brian <njugunajb96@gmail.com>
2023-05-22 23:32:53 +0300

    create login component

---
+++ frontend/src/app/modules/feature/auth/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  isLoading: boolean = false;
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+}
