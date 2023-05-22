9f1f9b5c286c5eaaf652a49e8d9212ba49b60621
John Brian <njugunajb96@gmail.com>
2023-05-22 23:33:15 +0300

    create register component

--- frontend/src/app/modules/feature/auth/register/register.component.ts
+++ frontend/src/app/modules/feature/auth/register/register.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  registrationForm: FormGroup;
+  isLoading: boolean = false;
+  constructor(private formBuilder: FormBuilder) {
+    this.registrationForm = this.formBuilder.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
 
+  handleRegister() {
+    this.isLoading = true;
+    if (this.registrationForm.invalid) {
+      this.isLoading = false;
+      return this.registrationForm.markAllAsTouched();
+    }
+  }
 }
