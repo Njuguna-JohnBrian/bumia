8417a0316b91df21ab392c6ff359f1cfbc868f0e
John Brian <njugunajb96@gmail.com>
2023-05-22 23:32:21 +0300

    add modules

--- frontend/src/app/modules/feature/auth/auth.module.ts
+++ frontend/src/app/modules/feature/auth/auth.module.ts
@@ -3,15 +3,31 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterComponent } from './register/register.component';
-
+import {
+  TuiInputModule,
+  TuiInputPasswordModule,
+  TuiIslandModule,
+} from '@taiga-ui/kit';
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  TuiButtonModule,
+  TuiErrorModule,
+  TuiTextfieldControllerModule,
+} from '@taiga-ui/core';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [
-    RegisterComponent
-  ],
+  declarations: [RegisterComponent, LoginComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
-  ]
+    AuthRoutingModule,
+    TuiIslandModule,
+    ReactiveFormsModule,
+    TuiInputModule,
+    TuiTextfieldControllerModule,
+    TuiButtonModule,
+    TuiInputPasswordModule,
+    TuiErrorModule,
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}
