4581869d389aec3abb49845775ec6d2d4d41b964
John Brian <njugunajb96@gmail.com>
2023-10-11 03:58:02 +0300

    add seed utility file

--- backend/utils/seeder.utils.ts
+++ backend/utils/seeder.utils.ts
@@ -1,52 +1,20 @@
-import dotenv from "dotenv";
-import * as path from "path";
-
-import { testProducts, adminUser } from "../data/products.data";
-import { connectDatabase } from "../config/database";
+import { adminUser, testProducts } from "../data/seed.data";
 import { Product } from "../models/product.model";
 import { User } from "../models/users.model";
 
-dotenv.config({
-  path: path.join(__dirname, "../../../backend/config/config.env"),
-});
-
-connectDatabase();
-
 const seedProducts = async (): Promise<void> => {
-  try {
-    await Product.deleteMany();
-
-    console.log("All products deleted from database...");
-
-    await Product.insertMany(testProducts);
-
-    console.log("All products added to database...");
-
-    process.exit();
-  } catch (error) {
-    console.log(error.message);
-
-    process.exit();
-  }
+  await Product.deleteMany();
+  console.log("All products deleted from database...");
+  await Product.insertMany(testProducts);
+  console.log("All products added to database...");
 };
 
 const seedAdminUser = async (): Promise<void> => {
-  try {
-    await User.deleteMany();
-
-    console.log("All users deleted from database...");
-
-    await User.insertMany(adminUser);
-
-    console.log("All users added to database...");
-
-    process.exit();
-  } catch (error) {
-    console.log(error.message);
-
-    process.exit();
-  }
+  await User.deleteMany();
+  console.log("All users deleted from database...");
+  await User.insertMany(adminUser);
+  console.log("All users added to database...");
 };
 
-seedAdminUser();
-seedProducts();
+
+export {seedAdminUser, seedProducts}
